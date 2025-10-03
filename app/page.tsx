820360ec965f125c959119b8d2b76f9391f56806
kaio-maya <klucca190@gmail.com>
2025-10-03 08:14:22 -0300

    estrutura inicial do projeto

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,21 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center bgt-gray-100">
+      <header className="text-center p-8 bg-black rounded-lg shadow-lg w-full mb-100 max-w-5xl ">
+        <nav>
+          <h1 className="text-4xl font-bold text-white">Meu Site Next.js</h1>
+        </nav>
+      </header>
+      <div className="text-center p-8 bg-white rounded-lg shadow-lg w-full max-w-3xl h-96">
+
       <h1 className="text-6xl font-bold text-blue-600">
         Minha Primeira Página com Next.js!
       </h1>
-      <p className="mt-4 text-xl">
+      <p className="mt-4 text-xl bg-gray p-8 rounded-lg shadow-md text-black">
         Começando minha jornada no desenvolvimento web moderno.
       </p>
-      <p className="font-bold text-red-500">
-        Olá, mundo!!
-      </p>
+      </div>
     </main>
   );
 }
