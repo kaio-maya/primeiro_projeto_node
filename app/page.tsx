bd13569d98820665291bd4a1b742fb7aea4669f4
kaio-maya <klucca190@gmail.com>
2025-10-03 08:33:35 -0300

    qwer

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bgt-gray-100">
-      <header className="text-center p-8 bg-black rounded-lg shadow-lg w-full mb-100 max-w-5xl ">
+      <div>
+      <header className="text-center p-8 bg-amber-800 rounded-lg shadow-2xl w-full mb-8 max-w-10xl ">
         <nav>
           <h1 className="text-4xl font-bold text-white">Meu Site Next.js</h1>
         </nav>
       </header>
+      </div>
       <div className="text-center p-8 bg-white rounded-lg shadow-lg w-full max-w-3xl h-96">
 
       <h1 className="text-6xl font-bold text-blue-600">
