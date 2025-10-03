8b2a920003780c8fff338e936d1b1630ec464cd8
kaio-maya <klucca190@gmail.com>
2025-10-03 09:34:25 -0300

    sdsa

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,22 @@
 // Arquivo: app/page.tsx
+import GraficoTradingView from '@/components/GraficoTradingView';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bgt-gray-100">
       <div>
-      <header className="text-center p-8 bg-amber-800 rounded-lg shadow-2xl w-full mb-8 max-w-10xl ">
+      <header className="text-center p-8 bg-amber-800 rounded-lg shadow-2xl  mb-8 w-3xl">
         <nav>
           <h1 className="text-4xl font-bold text-white">Meu Site Next.js</h1>
         </nav>
       </header>
       </div>
-      <div className="text-center p-8 bg-white rounded-lg shadow-lg w-full max-w-3xl h-96">
+      <div className="text-center p-8 bg-white rounded-lg shadow-lg w-full max-w-3xl h-200 ">
 
       <h1 className="text-6xl font-bold text-blue-600">
         Minha Primeira Página com Next.js!
       </h1>
-      <p className="mt-4 text-xl bg-gray p-8 rounded-lg shadow-md text-black">
-        Começando minha jornada no desenvolvimento web moderno.
-      </p>
+      <GraficoTradingView />
       </div>
     </main>
   );
