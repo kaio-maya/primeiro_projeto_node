d332786a0f069d2306e88fba501da1f07db57083
kaio-maya <klucca190@gmail.com>
2025-10-06 07:45:55 -0300

    mudança de cores

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bgt-gray-100">
       <div>
-      <header className="text-center p-8 bg-amber-800 rounded-lg shadow-2xl  mb-8 w-3xl">
+      <header className="text-center p-8 bg-amber-800 rounded-lg shadow-2xl  mb-4 w-4xl">
         <nav>
-          <h1 className="text-4xl font-bold text-white">Meu Site Next.js</h1>
+          <h1 className="text-4xl font-bold text-white">Financias</h1>
         </nav>
       </header>
       </div>
-      <div className="text-center p-8 bg-amber-50 bg-gradient-to-l rounded-lg shadow-lg w-full max-w-3xl h-200 ">
+      <div className="text-center p-8 bg-amber-50 bg-gradient-to-l rounded-lg shadow-lg w-full max-w-9xl h-190 ">
 
       <h1 className="text-6xl font-bold text-blue-600">
         Pesquisa de Ações
